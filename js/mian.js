f53b4435f71cbd08ebb67d290fb8d6db2e631303
Abbas64Ahmed <ffrree64@gmail.com>
2025-01-24 16:14:35 +0300

    upload the projict

---
+++ js/mian.js
@@ -0,0 +1,16 @@
+// const stickyElement = document.getElementById("header");
+// console.log(stickyElement);
+
+// window.addEventListener("scroll", () => {
+//   const scrollPosition = window.scrollY;
+//   console.log(scrollPosition);
+
+//   // إظهار العنصر عند التمرير لمسافة معينة
+//   if (scrollPosition > 600) {
+//     stickyElement.style.display = "fixed";
+//     console.log("fixed");
+//   } else {
+//     stickyElement.style.display = "absolute";
+//     console.log("absolute");
+//   }
+// });
